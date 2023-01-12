f696da12f434119cf8dfb19982d0e7abd2798e22
l1terally <literallyuriy@gmail.com>
2023-01-12 13:06:58 +0800

    Initial Commit

---
+++ contactss/contactssApp.swift
@@ -0,0 +1,17 @@
+//
+//  contactssApp.swift
+//  contactss
+//
+//  Created by Юрий  Лизунов on 12/01/23.
+//
+
+import SwiftUI
+
+@main
+struct contactssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
